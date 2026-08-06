0477142275dd38ef09ccdae7e9d2c03f34c104b0
TipcyTi <181719697+TipcyTi@users.noreply.github.com>
2026-08-06 11:47:24 +0800

    Create scan.js

---
+++ scan.js
@@ -0,0 +1,9 @@
+const const fs = require('fs');
+const files = fs.readdirSync('./public/images')
+  .filter(f => /\.(jpg|jpeg|png|gif|webp|svg|avif)$/i.test(f));
+
+fs.writeFileSync('./public/images.json', JSON.stringify({
+  images: files.map(name => ({ name, url: `/images/${name}` }))
+}, null, 2));
+
+console.log('Scanned', files.length, 'images');
